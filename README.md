82f44c93abdef8ecda10c42aacaeda6114c4f6a3
Milosz Szczepaniak <cryptobts7@gmail.com>
2024-02-21 15:17:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# ToDo Web App
+
+## Opis
+Prosta aplikacja webowa ToDo, która umożliwia dodawanie nowych zadań do listy oraz śledzenie postępu.
+
+## Funkcje
+
+- Dodawanie nowych zadań do listy
+- Wyświetlanie listy zadań
+- Podstawowe statystyki dotyczące ilości zadań
+
+## Struktura projektu:
+|-- index.html
+|-- css/
+| |-- style.css
+| |-- normalize.css
+| |-- container.css
+| |-- section.css
+| |-- form.css
+| |-- tasks.css
+|-- js/
+| |-- script.js
+|-- README.md
+
+## Instrukcje uruchomienia
+1.Pobierz pliki z tego repozytorium.
+2.Uruchom plik index.html w dowolnej wspieranej przeglądarce internetowej.
+## Zależności
+Brak zewnętrznych zależności. Projekt korzysta tylko z podstawowych technologii webowych.
+
+## Autor
+[Twoje Imię/Nazwa]
+
+## Kontakt
+Jeśli masz pytania lub uwagi, skontaktuj się ze mną poprzez [wpisz adres e-mail lub inne informacje kontaktowe].
---
+++ js/script.js
@@ -0,0 +1,83 @@
+{
+  const tasks = [];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({ content: newTaskContent });
+    render();
+  };
+
+  const removeTask = (index) => {
+    tasks.splice(index, 1);
+    render();
+  };
+
+  const toggleTaskDone = (index) => {
+    tasks[index].done = !tasks[index].done;
+    render();
+  };
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  };
+
+  const render = () => {
+    let tasksListContent = "";
+
+    for (const task of tasks) {
+      tasksListContent += `
+              <li class="tasks__item js__task">
+              <button class="tasks__button tasks__button--done js-done">${task.done ? "✓" : ""}</button>
+              <span class="tasks__content${ task.done ? "tasks__content--done" : ""}">${task.content}</span>
+              <button class="tasks__button tasks__button--remove js-remove">🗑️</button>
+              </li>
+          `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = tasksListContent;
+
+    document.querySelector(".js-stats").innerText = `
+          Liczba wszystkich zadań: ${tasks.length}\n
+          Liczba ukończonych zadań: ${tasks.filter((task) => task.done).length}
+      `;
+
+    bindEvents();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskElement = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskElement.value.trim();
+
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
+      newTaskElement.value = "";
+    }
+
+    newTaskElement.focus();
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
